b44fc5da11d3cc9004f27ef179bc84b5c26b6935
John Austin <kleptine@gmail.com>
2016-01-17 22:33:41 -0800

    Prototype of viewing a spiral manifold interaction.

---
+++ Spiralfold/Assets/SliceViewer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Utilities;
+
+public class SliceViewer : MonoBehaviour {
+    private const float SLICE_NUM = 4;
+
+    // A list of slices that we can toggle on and off.
+    private readonly List<Slice> slices = new List<Slice>();
+
+    // This is our position on the looping manifold. (-inf, inf)
+    private float manifold_translation = 0;
+
+    private Vector3 previous_forward;
+
+    private struct Slice {
+        public GameObject g_object;
+        public float start_angle;
+        public float end_angle;
+
+        public float Midpoint() {
+            return (start_angle + end_angle) / 2.0f;
+        }
+    }
+
+	protected void Start () {
+	    previous_forward = transform.forward;
+
+        // Add all of the slices
+	    for (int i = 0; i < SLICE_NUM; i++) {
+            slices.Add(new Slice {
+                g_object = GameQuery.FindOrThrow("Half_"+i, find_non_active:true),
+                start_angle = i * 180.0f,
+                end_angle = (i+1) * 180.0f,
+            });
+	    }
+	}
+	
+	protected void Update () {
+	    float rotation_change = Math.AngleSigned(previous_forward, transform.forward, Vector3.up);
+	    previous_forward = transform.forward;
+
+        // Move us along the manifold
+	    manifold_translation += rotation_change;
+
+        // Show any slice with a midpoint that is within 180 degrees of our 
+        // look direction / position in the manifold.
+	    foreach (Slice s in slices) {
+            bool active = Mathf.Abs(s.Midpoint() - manifold_translation) < 180.0f; 
+            s.g_object.SetActive(active);
+	    }
+	}
+}
---
+++ Spiralfold/Assets/Utilities/GameQuery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utilities {
+    public static class GameQuery {
+        public static GameObject FindOrThrow(string name, bool find_non_active = false) {
+            if (find_non_active) {
+                // To find a non active object we have to use a separate, but most likely slower process:
+                foreach (GameObject g in Resources.FindObjectsOfTypeAll(typeof (GameObject))) {
+                    if (g.name == name) {
+                        return g;
+                    }
+                }
+
+                throw new ArgumentException("No game object with name: " + name);
+            } else {
+                var obj = GameObject.Find(name);
+                if (obj == null) {
+                    throw new ArgumentException("No game object with name: " + name);
+                }
+                return obj;
+            }
+        }
+    }
+}
