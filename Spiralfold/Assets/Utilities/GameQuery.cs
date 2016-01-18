817d64cbd228cec6b1158e6e305dd4493f8a3a8d
John Austin <kleptine@gmail.com>
2016-01-18 00:33:27 -0800

    More fun with the different slices.

--- Spiralfold/Assets/Utilities/GameQuery.cs
+++ Spiralfold/Assets/Utilities/GameQuery.cs
@@ -3,6 +3,16 @@ using UnityEngine;
 
 namespace Utilities {
     public static class GameQuery {
+
+        /// <summary>
+        /// Finds a <see cref="GameObject"/> in the scene by the matching
+        /// <paramref name="name"/> or throws an exception if not found.
+        /// </summary>
+        /// <param name="name">Name of <see cref="GameObject"/></param>
+        /// <param name="find_non_active">
+        /// Whether to search for non-active objects as well. Note this may be
+        /// slower.
+        /// </param>
         public static GameObject FindOrThrow(string name, bool find_non_active = false) {
             if (find_non_active) {
                 // To find a non active object we have to use a separate, but most likely slower process:
